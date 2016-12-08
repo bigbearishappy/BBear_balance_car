e3c027707ddf3d56c13a3a64aa0cd64d82d9c308
bigbearishappy <953308023@qq.com>
2016-12-08 23:14:27 +0800

    delete the old code of the project
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- source_code/source_code/APP/filter.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILTER_H
-#define FILTER_H
-
-#include<math.h>
-
-float Kaerman_Filter(float data_filted, float radian, float radian_pt);
-float Kalman_Filter(float measure_value, float measure_cur_err, float previous_best, float pre_best_err, float current_fore_err);
-
-#endif
--- source_code/source_code/BSP/HX1838.h
+++
@@ -1,9 +0,0 @@
-#ifndef HX1838_H
-#define HX1838_H
-#include "stm32f10x.h"
-
-extern unsigned int remote_data;
-
-void HX1838_Init(void);
-
-#endif
